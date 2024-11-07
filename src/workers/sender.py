1ef2f2e93019ccc56c51afbff37e44b124a6cd64
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2024-11-07 18:29:34 +0100

    feat: add sender

---
+++ src/workers/sender.py
@@ -0,0 +1,65 @@
+import logging
+import os
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+import markdown
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Sender:
+    def __init__(self):
+        self.smtp_server = os.getenv("IMAP_SERVER")
+        self.port = int(os.getenv("SMTP_PORT", 465))
+        self.email_account = os.getenv("EMAIL_ACCOUNT")
+        self.password = os.getenv("PASSWORD")
+
+    def connect_to_smtp(self):
+        try:
+            server = smtplib.SMTP_SSL(self.smtp_server, self.port)
+            server.login(self.email_account, self.password)
+            logging.info("Connected to SMTP server successfully.")
+            return server
+        except smtplib.SMTPException as e:
+            logging.error(f"SMTP connection error: {e}")
+            raise e
+
+    @staticmethod
+    def markdown_to_html(text):
+        return markdown.markdown(text)
+
+    def send_email(self, mail_id, to_address, subject, body):
+        server = None
+        try:
+            server = self.connect_to_smtp()
+            html_body = self.markdown_to_html(body)
+
+            message = MIMEMultipart("alternative")
+            message["From"] = self.email_account
+            message["To"] = to_address
+            message["Subject"] = subject
+
+            message.attach(MIMEText(body, "plain"))
+            message.attach(MIMEText(html_body, "html"))
+
+            server.sendmail(self.email_account, to_address, message.as_string())
+            logging.info(f"Email sent to {to_address}")
+            return {"id": mail_id, "recipient": to_address, "success": True, "error": None}
+
+        except Exception as e:
+            logging.error(f"Error sending email: {e}")
+            return {"id": mail_id, "recipient": to_address, "success": False, "error": str(e)}
+
+        finally:
+            if server:
+                server.quit()
+
+    def send_emails(self, mail_id, recipients, subject, body) -> list[dict]:
+        responses = []
+        for recipient in recipients:
+            response = self.send_email(mail_id, recipient, subject, body)
+            responses.append(response)
+        return responses
